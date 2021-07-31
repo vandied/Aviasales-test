e5df9dd2ee63bff6bcf95d54446094c480ed8802
vandied <vandiedarling@gmail.com>
2021-07-31 18:00:43 +0300

    layout fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react';
+import Logo from '/assets/images/logo.svg';
 import css from './styles/styles.css';
 import { TransferFilter, TicketToggle, TicketList } from './components';
 
 export const App: FC = () => (
-    <div className={css.searchingPage}>
-        <TransferFilter />
-        <div className={css.searchingResults}>
-            <TicketToggle />
-            <TicketList />
+    <div className={css.searchingRoot}>
+        <img src={Logo} alt="logo" className={css.searchingLogo} />
+        <div className={css.searchingPage}>
+            <TransferFilter />
+            <div className={css.searchingResults}>
+                <TicketToggle />
+                <TicketList />
+            </div>
         </div>
     </div>
 );
--- src/components/TransferFilter/consts.ts
+++ src/components/TransferFilter/consts.ts
@@ -3,4 +3,5 @@ export const transferOptions = [
     { id: 1, title: 'Без пересадок' },
     { id: 2, title: '1 пересадка' },
     { id: 3, title: '2 пересадки' },
+    { id: 3, title: '3 пересадки' },
 ];
